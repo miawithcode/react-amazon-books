b0349bebfc5659e03d0ed8a964b248f89afc38ee
miawithcode <cuttontail@gmail.com>
2023-12-17 22:24:42 +0800

    feat: use props in right way

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,7 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const {img, title, author} = props;
+const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
       <img
